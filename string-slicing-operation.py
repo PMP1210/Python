3fc237a1d8818089a03cea84e066ebf525b33fad
Pratham Parekh <127593837+PMP1210@users.noreply.github.com>
2024-10-28 14:02:08 +0530

    slicing using index

---
+++ string-slicing-operation.py
@@ -0,0 +1,12 @@
+str1="hello , i am a python developer." # its a string
+
+print(str1[0:7]) # it will print the content of str1 from index 0 to 6
+print(str1[0:])  # it will print the all the content from 0th index
+print(str1[:4]) #it will print the content from 0 to 3 index
+print(str1[0:-7]) # it will print the content from 0 to [length-7]th (0 to 24) index
+print(str1[-3:-1]) # it will print the content from [length-3]th to [length-1]th (28 to 31) index
+
+nm="Harry"
+print(nm[-4:-2]) # a to r
+
+print("String length is :" , len(str1)) # it will print the lenght of the str1
